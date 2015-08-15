49c4d9df3518287eb12373622cce61610d4b203e
theone <297190869@qq.com>
2015-08-15 15:31:19 +0800

    url remove hash,and #0.1.7

--- app/controllers/admin/login.js
+++ app/controllers/admin/login.js
@@ -50,54 +50,56 @@ exports.verify = {
         async.waterfall([
           function (cb) {
             //- 测试
-            // help.facePlusPlusDetect('http://res.cloudinary.com/theone/image/upload/v1425302230/mavjc53omfyfnngyjebw.jpg', cb);
+            help.facePlusPlusDetect('http://res.cloudinary.com/theone/image/upload/v1425302230/mavjc53omfyfnngyjebw.jpg', cb);
+
             //- theone.io
-            help.facePlusPlusDetect('http://www.theone.io/data/tmp/'+img.name , cb);
+            // help.facePlusPlusDetect('http://www.theone.io/data/tmp/'+img.name , cb);
           },
           function (detectInfo, cb) {
             if(!detectInfo || !detectInfo.face || !detectInfo.face[0] ||!detectInfo.face[0].face_id){
               return cb({err:'no face_id'});
             }
-            // console.log(detectInfo.face[0].face_id);
+            console.log(detectInfo.face[0].face_id);
             help.faceRecongnitionVerify(detectInfo.face[0].face_id, 'ffc4dc1a7f5b117bb529a0f9509c75b7', cb);
           },
           function  (result ,cb) {
-            console.log(result);
+            
             // 返回对比差异
-            if (result.confidence > 50 && result.is_same_person ) {
+            if (result.confidence > 80 && result.is_same_person ) {
               req.session.userId = '54ede26288d1cb84097a886e';
               cb();
             } else { 
-              cb({is_login: false});
+              cb(null, {is_login: false});
             }
           },function (cb) {
             // 清空文件夹
             //********** 临时同步方法
-            
-            // var tmpfiles,
-            // dirPath = config.root+'/data/tmp/';
-            // try { 
-            //   tmpfiles = fs.readdirSync(dirPath); 
-            //   }catch(e) {
-            //      cb({'clear':'error'});
-            //   }
+/*
+            var tmpfiles,
+            dirPath = config.root+'/data/tmp/';
+            try { 
+              tmpfiles = fs.readdirSync(dirPath); 
+              }catch(e) {
+                 cb({'clear':'error'});
+              }
 
-            // if (tmpfiles.length > 0){
-            //   for (var i = 0; i < tmpfiles.length; i++) {
-            //     var filePath = dirPath + '/' + tmpfiles[i];
-            //     if (fs.statSync(filePath).isFile()) {
-            //       fs.unlinkSync(filePath);
-            //     }
-            //   }
-            // }
+            if (tmpfiles.length > 0){  
+              for (var i = 0; i < tmpfiles.length; i++) {
+                var filePath = dirPath + '/' + tmpfiles[i];
+                if (fs.statSync(filePath).isFile()) {
+                  fs.unlinkSync(filePath);
+                }
+              }
+            }
+*/
             cb();
           }
           ],function (err) {
           if(err){
-            res.json({is_login:false});
-          }else{
-            res.json({is_login:true});
+            console.log(err);
+            return next(err);
           }
+          res.json({is_login:true});
           res.end();
         });
 
